495298439aea9c266dcfdfe8eba17e0db25b503a
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-10-04 14:17:40 -0700

    Updates

---
+++ precache-manifest.ebc305da996ca122bee05b1ec4f6f012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d98152bb5c663d2352bacf7465dd961",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ae5d3465bd0aca3a6147",
+    "url": "/todolist/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "48c5759a253dcdc3edae",
+    "url": "/todolist/static/js/2.29fd03a1.chunk.js"
+  },
+  {
+    "revision": "ae5d3465bd0aca3a6147",
+    "url": "/todolist/static/js/main.343def7c.chunk.js"
+  },
+  {
+    "revision": "f3006b5cf8a4dfaf90bf",
+    "url": "/todolist/static/js/runtime-main.9ec888a7.js"
+  }
+]);
